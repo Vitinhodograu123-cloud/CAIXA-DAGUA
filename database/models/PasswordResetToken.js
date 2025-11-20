0c6194f03073682070424ed892064f8c417dd1c9
Vitinhodograu123-cloud <gitcopilot0@gmail.com>
2025-11-20 15:40:53 -0300

    Create PasswordResetToken model for MongoDB

---
+++ database/models/PasswordResetToken.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const passwordResetTokenSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  token: {
+    type: String,
+    required: true
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+    default: () => new Date(Date.now() + 1 * 60 * 60 * 1000) // 1 hora
+  }
+}, {
+  timestamps: true
+});
+
+// Remove tokens expirados automaticamente
+passwordResetTokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+
+module.exports = mongoose.model('PasswordResetToken', passwordResetTokenSchema);
