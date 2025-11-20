e4a1b8a787edaef0d38cc5493d141a2f593985b7
Vitinhodograu123-cloud <gitcopilot0@gmail.com>
2025-11-20 17:08:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/api/auth', require('./routes/passwordReset'));
+app.use('/api/tickets', require('./routes/tickets'));
 
 // Conexão com MongoDB - Versão para Render
 const connectDB = async () => {
@@ -915,6 +916,7 @@ server.listen(PORT, '0.0.0.0', () => {
 });
 
 module.exports = app;
+
 
 
 
